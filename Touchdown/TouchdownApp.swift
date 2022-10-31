8cf98ebe6a077700f4213b4fdb85d95678668b6c
Mateus <105805491+mateusmarinhoo@users.noreply.github.com>
2022-10-30 23:54:40 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Mateus Marinho on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
